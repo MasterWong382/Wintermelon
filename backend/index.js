a3cc47c0efa0d42526c56abec94abf461ece4887
MasterWong382 <masterwong382@gmail.com>
2022-11-30 15:00:46 +0800

    insert firebase

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 const { response } = require("express");
 const express = require("express");
 
@@ -14,3 +16,20 @@ app.get("/createAccount", (request, response) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADAeM4ppgLn6q_-hKc24s9kY1SDa0cpdc",
+  authDomain: "wintermelon-db.firebaseapp.com",
+  projectId: "wintermelon-db",
+  storageBucket: "wintermelon-db.appspot.com",
+  messagingSenderId: "271443258606",
+  appId: "1:271443258606:web:1c638678684376b4c317ff",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
