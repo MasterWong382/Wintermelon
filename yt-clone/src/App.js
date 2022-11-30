c067bafc65b2203f99fef20b62185ebeae6c8ea6
Samuel P <samuel_png10@hotmail.com>
2022-11-30 15:23:41 +0800

    add basic styling

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -1,34 +1,19 @@
-import './App.css';
+// import './App.css';
 
-function App() {
+import { ThemeProvider } from 'styled-components';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+const App = () => {
   return (
-    <div className="App">
-      <div className="App-Inner">
-        <img
-          className="Gay"
-          src="https://d1hjkbq40fs2x4.cloudfront.net/2016-07-16/files/cat-sample_1313.jpg"
-          alt="Not found"
-        ></img>
-        <h5>
-          Username: &nbsp;
-          <input
-            className="search-box"
-            type="search"
-            placeholder="Enter Username..."
-          />
-        </h5>
-        <h5>
-          Password: &nbsp;
-          <input
-            type="password"
-            className="search-box"
-            placeholder="Enter Password..."
-          />
-        </h5>
-        <button class="w3-container w3-light-grey w3-justify">Log In</button>
-      </div>
-    </div>
+    <ThemeProvider
+      theme={{
+        font: 'Fira Sans',
+      }}
+    >
+      <Login />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ yt-clone/src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import '../styles/Login.css';
+
+const Login = () => {
+  return (
+    <div className="App">
+      <div className="App-Inner">
+        <img
+          className="Gay"
+          src="https://d1hjkbq40fs2x4.cloudfront.net/2016-07-16/files/cat-sample_1313.jpg"
+          alt="Not found"
+        ></img>
+        <h5>
+          Username: &nbsp;
+          <input
+            className="search-box"
+            type="search"
+            placeholder="Enter Username..."
+          />
+        </h5>
+        <h5>
+          Password: &nbsp;
+          <input
+            type="password"
+            className="search-box"
+            placeholder="Enter Password..."
+          />
+        </h5>
+        <button class="w3-container w3-light-grey w3-justify">Log In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
