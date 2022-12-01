335ae6157c9f677a0dddb313ac2ad060d3366c18
Benjamin Toh <78788899+kwaraa@users.noreply.github.com>
2022-12-01 16:46:59 +0800

    login info

--- yt-clone/src/pages/Login.jsx
+++ yt-clone/src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import "../styles/Login.css";
 import React from "react";
+import {useRef} from 'react';
 
 const Login = () => {
+  const inputUsername = useRef(null);
+  const inputPassword = useRef(null);
+
+  function handleClick(){
+    console.log(inputUsername.current.value);
+    console.log(inputPassword.current.value);
+  }
   return (
     <div className="App">
       <div className="App-Inner">
@@ -13,6 +21,7 @@ const Login = () => {
         <h5>
           Username: &nbsp;
           <input
+            ref = {inputUsername}
             className="search-box"
             type="search"
             placeholder="Enter Username..."
@@ -21,12 +30,13 @@ const Login = () => {
         <h5>
           Password: &nbsp;
           <input
+            ref = {inputPassword}
             type="password"
             className="search-box"
             placeholder="Enter Password..."
           />
         </h5>
-        <button class="w3-container w3-light-grey w3-justify">Log In</button>
+        <button onClick = {handleClick} class="w3-container w3-light-grey w3-justify">Log In</button>
       </div>
     </div>
   );
