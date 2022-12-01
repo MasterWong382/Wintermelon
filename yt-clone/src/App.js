c15958df3db4c7d718f88f08eb40914b728cebd9
Benjamin Toh <78788899+kwaraa@users.noreply.github.com>
2022-12-01 16:13:53 +0800

    App.js updates

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -1,23 +1,5 @@
 // import './App.css';
 
-<<<<<<< Updated upstream
-import { ThemeProvider } from 'styled-components';
-import SidebarWrapper from './layouts/sidebar';
-import Sidebar from './layouts/sidebar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-
-const App = () => {
-  return (
-    <ThemeProvider
-      theme={{
-        font: 'Fira Sans',
-      }}
-    >
-      {/* <Login /> */}
-    <Sidebar />
-    </ThemeProvider>
-=======
 import { ThemeProvider } from "styled-components";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -31,13 +13,11 @@ const App = () => {
             <IndexRoute indexElement={<Home />} />
             <Route path="Login" element={<Login />} />
           </Route> */}
-        <Route element={<Home />}>
-          <Route path="/" element={<Test />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-        </Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        
       </Routes>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 };
 
